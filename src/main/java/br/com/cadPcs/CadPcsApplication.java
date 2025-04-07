acb5371afa889673795daa01a5f403e67c45095d
luizbati <luizcb1944@hotmail.com>
2025-04-07 19:15:23 -0300

    first commit

---
+++ src/main/java/br/com/cadPcs/CadPcsApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadPcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadPcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadPcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cadPcs/CadPcsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cadPcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadPcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
